9891f3b58b34e390a00d8f7560fc65e6c375995f
Prashantg143 <prashantg@geekyants.com>
2025-09-20 02:29:57 +0530

    update yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+   reporter: [
+            ['list'],
+            ['allure-playwright', { outputFolder: 'allure-results' }],
+            ['@test-reporter/playwright', {
+              reportPath: 'extent-report/report.html',
+              // ...other Extent options
+            }]
+          ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
